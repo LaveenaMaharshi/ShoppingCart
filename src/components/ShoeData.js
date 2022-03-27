f1b692d45ff5cafaabdc1e748048b0974f3a8733
laavinaamahharshi@gmail.com <laavinaamahharshi@gmail.com>
2022-03-28 00:41:56 +0530

    initial commit

---
+++ src/components/ShoeData.js
@@ -0,0 +1,63 @@
+import ShoeOne from '../img/shoe1.png';
+import ShoeTwo from '../img/shoe2.png';
+import ShoeThree from '../img/shoe3.png';
+import ShoeFour from '../img/shoe4.png';
+import ShoeFive from '../img/shoe5.png';
+import ShoeSix from '../img/shoe6.png';
+import ShoeSeven from "../img/shoe7.png";
+import ShoeEight from "../img/shoe8.png";
+import ShoeNine from "../img/shoe9.png";
+import ShoeTen from "../img/shoe10.png";
+import ShoeEleven from "../img/shoe11.png";
+import ShoeTwelve from "../img/shoe12.png";
+
+export default [
+  {
+    cardImg: ShoeOne,
+    cardPrice: 699,
+  },
+  {
+    cardImg: ShoeTwo,
+    cardPrice: 399,
+  },
+  {
+    cardImg: ShoeThree,
+    cardPrice: 459,
+  },
+  {
+    cardImg: ShoeFour,
+    cardPrice: 379,
+  },
+  {
+    cardImg: ShoeFive,
+    cardPrice: 279,
+  },
+  {
+    cardImg: ShoeSix,
+    cardPrice: 199,
+  },
+  {
+    cardImg: ShoeSeven,
+    cardPrice: 659,
+  },
+  {
+    cardImg: ShoeEight,
+    cardPrice: 599,
+  },
+  {
+    cardImg: ShoeNine,
+    cardPrice: 499,
+  },
+  {
+    cardImg: ShoeTen,
+    cardPrice: 399,
+  },
+  {
+    cardImg: ShoeEleven,
+    cardPrice: 189,
+  },
+  {
+    cardImg: ShoeTwelve,
+    cardPrice: 279,
+  },
+];
